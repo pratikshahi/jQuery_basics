24301b5a7610baa3369714be4f2ea7f4e93cd22b
Pratik Shahi <shahipr0@gmail.com>
2021-03-19 09:34:25 +0530

    Keeping Style to CSS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // $(document).ready(function(){        use this .ready if your jqury and js script is in head section
-//     $("h1").css("color","red");      if jQuery and Js at end before body no need of .ready
+//     $("h1").css("color","red");      
 // })
 
-$("h1").css("color","red"); 
+//$("h1").css("color","red");           if jQuery and Js at end before body no need of .ready
+
+$("h1").addClass("bigTitle margin");

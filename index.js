0098a5255f50571b27435b5abfe17f1325235195
Pratik Shahi <shahipr0@gmail.com>
2021-03-19 09:17:39 +0530

    Jquery where to place script

---
+++ index.js
@@ -0,0 +1,5 @@
+// $(document).ready(function(){        use this .ready if your jqury and js script is in head section
+//     $("h1").css("color","red");      if jQuery and Js at end before body no need of .ready
+// })
+
+$("h1").css("color","red"); 
